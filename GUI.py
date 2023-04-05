a525ba6e77e31b483c5de40b9823abde73ac4090
brettlemley <brett.lemley@gmail.com>
2023-04-04 22:29:24 -0400

    Implemented Complete and Exit Buttons(has unexpected keyword error on line 46)

--- GUI.py
+++ GUI.py
@@ -5,11 +5,16 @@ label = sg.Text("Type in a To-Do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
-                      enable_events=True, size=[45,10])
+                      enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
 window = sg.Window('My To-Do App',
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout=[[label],
+                           [input_box, add_button],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
                    font=('Helvetica', 20))
 
 while True:
@@ -32,7 +37,16 @@ while True:
             index = todos.index(todo_to_edit)
             todos[index] = new_todo
             functions.write_todos(todos)
-            window['todos'].update(values=todos)
+            window['todos'].update(value=todos)
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(value=todos)
+            window['todo'].update(value='')
+        case "Exit":
+            break
         case "todos":
             window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
